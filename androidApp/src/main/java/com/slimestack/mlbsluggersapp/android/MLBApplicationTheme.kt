82bb97dd92ab15fc190e0459675facf33286c331
hendercine <james@slimeify.co>
2025-02-04 12:45:30 -0800

    [MLB-1]feat: add desired font

--- androidApp/src/main/java/com/slimestack/mlbsluggersapp/android/MLBApplicationTheme.kt
+++ androidApp/src/main/java/com/slimestack/mlbsluggersapp/android/MLBApplicationTheme.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -35,7 +37,7 @@ fun MLBApplicationTheme(
     }
     val typography = Typography(
         bodyMedium = TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = poppinsFontFamily,
             fontWeight = FontWeight.Normal,
             fontSize = 16.sp
         )
@@ -53,3 +55,15 @@ fun MLBApplicationTheme(
         content = content
     )
 }
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val fontName = GoogleFont("Poppins")
+
+val poppinsFontFamily = FontFamily(
+    Font(googleFont = fontName, fontProvider = provider)
+)
