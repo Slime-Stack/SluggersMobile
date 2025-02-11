5c5588ab5ea52ce0516685d3b39aee4317b9c770
hendercine <james@slimeify.co>
2025-02-11 14:03:08 -0800

    [MLB-3]style: setup constants for UI labels

---
+++ androidApp/src/main/java/com/slimestack/mlbsluggersapp/android/ui/utils/UIConstants.kt
@@ -0,0 +1,9 @@
+package com.slimestack.mlbsluggersapp.android.ui.utils
+
+object UIConstants {
+    const val SCENE_TRANSITION_LABEL = "scene transition"
+    const val CAPTION_TRANSITION_LABEL = "caption transition"
+    const val PROGRESS_INDICATOR_TRANSITION_LABEL = "progress indicator transition"
+    const val ES_LANGUAGE_CODE = "es"
+    const val JA_LANGUAGE_CODE = "ja"
+}
